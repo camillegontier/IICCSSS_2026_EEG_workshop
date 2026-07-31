b4ecf159f09cd69854bbaf71fee415d5834bc962
Camille Gontier <cam.gontier@gmail.com>
2026-07-31 12:19:13 +0200

    First versions

--- preprocessing_camille.py
+++ preprocessing_camille.py
@@ -11,124 +11,147 @@ Created on Mon Feb  2 13:28:40 2026
 import os
 import mne
 import pyprep
+import csv
 import numpy as np
-import pandas as pd
 from matplotlib import pyplot as plt
 from pathlib import Path
 # %matplotlib qt
 
-# Parameters ##################################################################
-
-idx_part = 2
-
-# Channel names, types and montage
-ch_name = ['Fp1', 'AF7', 'AF3', 'F1', 'F3', 'F5', 'F7', 'FT7', 'FC5', 'FC3',
-           'FC1', 'C1', 'C3', 'C5', 'T7', 'TP7', 'CP5', 'CP3', 'CP1', 'P1',
-           'P3', 'P5', 'P7', 'P9', 'PO7', 'PO3', 'O1', 'Iz', 'Oz', 'POz', 'Pz',
-           'CPz', 'Fpz', 'Fp2', 'AF8', 'AF4', 'AFz', 'Fz', 'F2', 'F4', 'F6',
-           'F8', 'FT8', 'FC6', 'FC4', 'FC2', 'FCz', 'Cz', 'C2', 'C4', 'C6',
-           'T8', 'TP8', 'CP6', 'CP4', 'CP2', 'P2', 'P4', 'P6', 'P8', 'P10',
-           'PO8', 'PO4', 'O2', 'left_ear', 'right_ear', 'EOG_up', 'EOG_down',
-           'EOG_left', 'EOG_right', 'hand_1', 'hand_2']
-ch_type = {'left_ear': 'misc', 'right_ear': 'misc',
-           'EOG_up': 'eog', 'EOG_down': 'eog', 'EOG_left': 'eog',
-           'EOG_right': 'eog', 'hand_1': 'emg', 'hand_2': 'emg'}
-montage = 'biosemi64'
-
-# Events
-event_dict = {'stimulus_go': 1,
-              'stimulus_no_go': 2,
-              'response': 4,
-              'start_probe': 8,
-              'end_probe': 16}
-
-# Spectral analysis
-param_psd = {'method': 'welch', 'fmin': 1., 'fmax': 100.}
-bands = {'Delta (1-4 Hz)': (1, 4),
-         'Theta (4-8 Hz)': (4, 8),
-         'Alpha (8-12 Hz)': (8, 12),
-         'Beta (12-30 Hz)': (12, 30),
-         'Gamma (30-45 Hz)': (30, 45)}
-
-# Bad channels description
-bad_desc_dict = {
-    'bad_all': 'every bad channels',
-    'bad_by_deviation': 'channels with abnormally high or low overall amplitudes',
-    'bad_by_hf_noise': 'channels with abnormally high amounts of high-frequency noise',
-    'bad_by_flat': 'channels that have near-flat signals',
-    'bad_by_nan': 'channels that contain NaN values',
-    'bad_by_correlation': 'channels that sometimes don’t correlate with any other channels',
-    'bad_by_SNR': 'channels that have a low signal-to-noise ratio'
-}
-
-# Filters and references
-hpf = 0.2  # (Hz)
-lpf = 30  # (Hz)
-notch = None  # (Hz)
-ref = ['left_ear', 'right_ear']
-
-# ICA Parameters
-ica_l_freq = 1  # ICA HPF (Hz)
-n_components = 0.99  # Set number of components explaining x% of variance
-random_state = 42  # Set ICA seed for reproductibility
-ica_method = 'fastica'
-decim = 4
-ica_measure = 'correlation'
-ica_thresh = 0.7
-
 # Data reading ################################################################
 
-mapping_part = {
-    1: "T19",
-    2: "T28",
-    3: "T40",
-    4: "T52",
-    5: "T54",
-}
-part_name = mapping_part[idx_part]
-dir_path = Path(os.getcwd(),"Data",part_name,"EEG", part_name + "_SART_prep.fif")
-raw = mne.io.read_raw_fif(dir_path, preload=True)
+dir_path = Path(os.getcwd(),"Data","sub-001_task-CBOFF_run-1_eeg.set")
+raw = mne.io.read_raw_eeglab(dir_path, preload=True)
+# raw.plot(
+#     duration=10,
+#     n_channels=20,
+#     decim=4,
+#     events=events,
+#     event_id=event_dict,
+#     event_color=None,
+#     theme='dark'
+    
+# )
+
+# Channel types ################################################################
+
+# Print channel names
+print(raw.info)
+# Channel names, types and montage
+ch_type = {'ECG': 'ecg'}
+montage = 'easycap-M10'
+print(raw.info.ch_names)
+raw.get_channel_types()
+
 # Map channel names and types
-ch_dict = dict(zip(raw.ch_names, ch_name))
-mne.rename_channels(raw.info, ch_dict)
 raw.set_channel_types(ch_type)
-raw.set_montage(montage, on_missing='ignore')
+print(raw.info)
+raw.get_channel_types()
+# Check number of channels and sampling rate
+# Montage #####################################################################
+
+montage = mne.channels.make_standard_montage("easycap-M1")
+montage.plot(sphere="eeglab")
+
+
+# raw.set_montage(montage, on_missing='ignore')
+print(raw.info)
 
 # Compute epochs ##############################################################
+events = mne.events_from_annotations(raw)[0]
+idx_trial_start = np.where(events[:,2]==2)[0]
 
-events = mne.find_events(raw, stim_channel="Status")
+# read behavior data
+# with open("DATA\sub-001_task-CBOFF_run-1_beh.tsv") as fd:
+    
+#     rd = csv.reader(fd, delimiter="\t")
+#     next(rd)
+#     row_idx = 0
+#     for row in rd:
+#         if int(row[9]) != row_idx:
+#             print(row_idx)
+#             tasktype = row[3]
+#             stimulus = row[10]
+#             RT = float(row[12])
+#             if tasktype == stimulus:
+#                 events[idx_trial_start[row_idx],2] = 10
+#             else:
+#                 events[idx_trial_start[row_idx],2] = 11
+#             if RT > 0:
+#                 events = np.vstack([events, [events[idx_trial_start[row_idx],0]+RT*raw.info['sfreq']/1000,0,12]])
+#             row_idx = row_idx + 1
+# # Events
+event_dict = {'stimulus_go': 10,
+              'stimulus_no_go': 11,
+              'response': 12}
+
+# events_fig = mne.viz.plot_events(
+#     events,
+#     event_id=event_dict,
+#     sfreq=raw.info['sfreq'],
+#     first_samp=raw.first_samp,
+#     on_missing='ignore'
+# )
+
 events_fig = mne.viz.plot_events(
     events,
-    event_id=event_dict,
     sfreq=raw.info['sfreq'],
     first_samp=raw.first_samp,
     on_missing='ignore'
 )
+# raw.plot(
+#     duration=10,
+#     n_channels=20,
+#     decim=4,
+#     events=events,
+#     event_id=event_dict,
+#     event_color=None,
+#     theme='dark'
+    
+# )
+# check number of trials
+# Filtering ###################################################################
 
-# Processing ##################################################################
+# Filters and references
+hpf = 0.2  # (Hz) # to remove drifts, before epochs for edge artifacts
+lpf = 100  # (Hz)
+notch = None  # (Hz)
+# Filters
+raw_filt = raw.copy().filter(hpf, lpf)
+raw_filt.notch_filter(notch) if notch is not None else None
 
-# BAD CHANNELS DETECTION
-raw.plot(
+
+
+# Referencing #################################################################
+# ref = ['left_ear', 'right_ear']
+raw.set_eeg_reference("average")
+# Usually average AFTER the bad channels
+# # Rereference
+# raw_reref = raw_filt.copy().set_eeg_reference(ref, ch_type='eeg')
+raw_reref = raw_filt.copy().set_eeg_reference("average", projection=False, verbose=False)
+
+raw_reref.plot(
     duration=10,
     n_channels=20,
     decim=4,
     events=events,
     event_id=event_dict,
     event_color=None,
-    theme='dark'
+    theme='dark',
+    block=False
 )
 
-# Spectrum analysis: plot broad-range spectra (lin+log scales) + topomaps
-# (Welch's method for computation efficiency)
-spectrum = raw.compute_psd(
-    **param_psd,
-    n_fft=round(raw.info['sfreq']) * 4  # length of Welch window (pts)
-)
-spec_fig, axs = plt.subplots(ncols=2, figsize=(16, 7), tight_layout=True, clear=True)
-axs[0] = spectrum.plot(axes=axs[0])
-axs[1] = spectrum.plot(xscale='log', axes=axs[1])
-spectrum.plot_topomap(bands=bands, cmap='Spectral_r', size=10.0)
-plt.show(block=True)
+
+# BAD CHANNELS DETECTION ##################################################################
+# raw.plot(
+#     duration=10,
+#     n_channels=20,
+#     decim=4,
+#     events=events,
+#     event_id=event_dict,
+#     event_color=None,
+#     theme='dark'
+    
+# )
 
 # PyPREP : find bad channels following specific criteria (correlation, deviation...)
 noisy_chan = pyprep.NoisyChannels(raw)
@@ -159,35 +182,27 @@ raw.plot(
     events=events,
     event_id=event_dict,
     event_color=None,
-    theme='dark',
-    block=True
+    theme='dark'
 )
 if raw.info['bads'] != []:
     bad_chan_fig = (raw.copy()
                     .pick(raw.info['bads'])
                     .plot_sensors(show_names=True))
     
-# Filters
-raw_filt = raw.copy().filter(hpf, lpf, picks=raw.ch_names[0:66])
-raw_filt.notch_filter(notch) if notch is not None else None
 
 
 
-# Rereference
-raw_reref = raw_filt.copy().set_eeg_reference(ref, ch_type='eeg')
-raw_reref = raw_filt.copy().set_eeg_reference("average", projection=False, verbose=False)
-
-raw_reref.plot(
-    duration=10,
-    n_channels=20,
-    decim=4,
-    events=events,
-    event_id=event_dict,
-    event_color=None,
-    theme='dark',
-    block=False
-)
 # %%% OCULAR CORRECTION WITH ICA
+
+# ICA Parameters
+ica_l_freq = 1  # ICA HPF (Hz)
+n_components = 0.99  # Set number of components explaining x% of variance
+random_state = 42  # Set ICA seed for reproductibility
+ica_method = 'fastica'
+decim = 4
+ica_measure = 'correlation'
+ica_thresh = 0.7
+
 # Perform ICA
 raw_preica = raw_reref.copy().filter(ica_l_freq, h_freq=None)
 epochs_preica = mne.make_fixed_length_epochs(raw_preica, duration=10)
@@ -233,4 +248,100 @@ ica.apply(raw_postica)
 
 # Interpolation (if bad channels detected)
 raw_postica.interpolate_bads(reset_bads=True) if raw_postica.info['bads'] else None
+
+# Balistocardiogram
+
+# Spectral analysis ############################################################
+
+# Epoch = add a dimension to the data
+
+
+hpf = 1 # (Hz)
+lpf = 40 # (Hz)
+notch = None # (Hz)
+# Filters
+raw.filter(hpf, lpf)
+raw.notch_filter(50)
+
+raw.pick(['Oz','O1','O2','POz'])
+
+# psd = raw.compute_psd(
+# method="welch",
+# fmin=1,
+# fmax=30,
+# tmin=60, 
+# tmax=73,
+# n_fft=8192
+# )
+
+# psd = raw.compute_psd(
+# method="multitaper",
+# fmin=5,
+# fmax=25,
+# bandwidth=0.1,
+# )
+# psd.plot()
+
+
+
+
+
+epochs = mne.Epochs(
+raw,
+events,
+event_id=1,
+tmin=0,
+tmax=15, # or whatever the stimulus duration is
+baseline=None,
+preload=True,
+)
+
+psd = epochs.compute_psd(
+method="multitaper",
+fmin=1,
+fmax=30,bandwidth=0.1,
+)
+
+psd.plot()
+ 
+33333333333333333333333333333333333333333333
+
+raw.drop_channels(["ECG"])
+
+spectrum = raw.compute_psd(method='welch', fmin=0, 
+fmax=100, 
+tmin=165, 
+tmax=180, 
+)
+spec_fig, axs = plt.subplots(ncols=2, figsize=(16, 7), tight_layout=True, clear=True)
+axs[0] = spectrum.plot(axes=axs[0],dB=True)
+axs[1] = spectrum.plot(xscale='log', axes=axs[1])
+plt.show()
+
+
+
+
+
+# # Spectral analysis
+param_psd = {'method': 'welch', 'fmin': 1., 'fmax': 100.}
+bands = {'Delta (1-4 Hz)': (1, 4),
+'Theta (4-8 Hz)': (4, 8),
+'Alpha (8-12 Hz)': (8, 12),
+'Beta (12-30 Hz)': (12, 30),
+'Gamma (30-45 Hz)': (30, 45)}
+
+# Spectrum analysis: plot broad-range spectra (lin+log scales) + topomaps
+# (Welch's method for computation efficiency)
+spectrum = raw.compute_psd(
+**param_psd,
+n_fft=round(raw.info['sfreq']) * 1 # length of Welch window (pts)
+)
+spec_fig, axs = plt.subplots(ncols=2, figsize=(16, 7), tight_layout=True, clear=True)
+axs[0] = spectrum.plot(axes=axs[0],dB=True)
+axs[1] = spectrum.plot(xscale='log', axes=axs[1])
+spectrum.plot_topomap(bands=bands, cmap='Spectral_r', size=10.0)
+plt.show(block=True)
+
+
+
 
